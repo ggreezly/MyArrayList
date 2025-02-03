294e30972019cdf38b68b3906a5ef3820c26b562
Ivan Bykhalo <ggreezly@gmail.com>
2025-02-03 19:20:43 +0300

    MyArrayList - это кастомная реализация динамического массива. Поддерживает основные операции, такие как добавление, удаление, получени,очистка и сортировка. Реализован алгоритм quicksort для реализованного MyArrayList.

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,28 @@ public class Main {
         for (int i = 0; i < stringList.size(); i++) {
             System.out.println(stringList.get(i));
         }
+
+        // Пример с большим количеством данных
+        // Добавление
+        MyArrayList<Integer> largeList = new MyArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            largeList.add((int) (Math.random() * 100000));
+        }
+
+        System.out.println("Size of largeList: " + largeList.size());
+
+        // Сортировка
+        long startTime = System.currentTimeMillis();
+        largeList.sort(Comparator.naturalOrder());
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Time taken to sort largeList: " + (endTime - startTime) + " ms");
+
+        // Удаление
+        for (int i = 0; i < 50000; i++) {
+            largeList.remove(0); // Удаляем первые 50000 элементов
+        }
+
+        System.out.println("Size of largeList after removal: " + largeList.size());
     }
 }
